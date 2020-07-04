782f5b7374827dbe16590966bd637a9ea9f37ab1
Florin Langer <florin@berkeley.edu>
2020-07-04 00:43:07 -0700

    fixed test

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
-import { ExampleComponent } from '.'
+import { OnboardElement } from '.'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+    expect(OnboardElement).toBeTruthy()
   })
 })
