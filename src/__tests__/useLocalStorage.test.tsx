29995d6607576a084da87e396fea2b35b4920bf1
Florin Langer <florin@berkeley.edu>
2021-09-10 14:33:25 -0400

    add local storage test

--- src/__tests__/useLocalStorage.test.tsx
+++ src/__tests__/useLocalStorage.test.tsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useLocalStorage } from '../useLocalStorage';
 
-const Component = () => {
-  const [value, setValue] = useLocalStorage('test', 1);
-  return <button type="button" data-testid="value" onClick={() => setValue((v) => v + 1)}>{value}</button>;
+const Component = ({ storageKey, initialValue }: {storageKey: string, initialValue?: number}) => {
+  const [value, setValue] = useLocalStorage(storageKey, initialValue);
+  return <button type="button" data-testid="value" onClick={() => setValue((v) => v ? v + 1 : 1)}>{value}</button>;
 };
 
 test('useLocalStorage has initial value and can be updated', () => {
-  render(<Component />);
+  render(<Component storageKey="1" initialValue={1} />);
   const value = screen.getByTestId('value');
   expect(value.textContent).toBe('1');
   fireEvent.click(value);
   expect(value.textContent).toBe('2');
+});
+
+test('useLocalStorage has no initial value and can be updated', () => {
+  render(<Component storageKey="2" />);
+  const value = screen.getByTestId('value');
+  expect(value.textContent).toBe('');
+  fireEvent.click(value);
+  expect(value.textContent).toBe('1');
 });
