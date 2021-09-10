e405d08e8395264921130e776b4f4b4429118ca6
Florin Langer <florin@berkeley.edu>
2021-09-10 02:45:16 -0400

    add test

--- src/__tests__/OnboardProvider.test.tsx
+++ src/__tests__/OnboardProvider.test.tsx
@@ -11,7 +11,7 @@ test('Onboard message is shown when it does not depend on any elements and can b
     return (
       <OnboardProvider
         messages={[{
-          id: '1',
+          id: 'this is a welcome',
           children: <div>this is a welcome</div>,
         }]}
         // eslint-disable-next-line jsx-a11y/interactive-supports-focus, jsx-a11y/click-events-have-key-events
@@ -29,5 +29,38 @@ test('Onboard message is shown when it does not depend on any elements and can b
   const message = await screen.findByTestId('message');
   expect(message.textContent).toBe('this is a welcome');
   fireEvent.click(message);
+  expect(screen.queryByTestId('message')).toBeNull();
+});
+
+test('Onboard message is shown when it depends on an already present element and can be dismissed by clicking element', async () => {
+  const Component = () => {
+    const [message, setMessage] = useState<React.ReactNode>(null);
+
+    return (
+      <OnboardProvider
+        messages={[{
+          id: 'this is a welcome with an element',
+          children: <div>this is a welcome with an element</div>,
+          elementIds: ['welcome element'],
+        }]}
+        showCallback={({ children }) => { setMessage(<div data-testid="message">{children}</div>); }}
+        ackCallback={() => { setMessage(null); }}
+        HighlightComponent={({ children, ...rest }) => <div data-testid="highlight-component" {...rest}>{children}</div>}
+      >
+        <OnboardElement id="welcome element">
+          <div>WELCOME</div>
+        </OnboardElement>
+        {message}
+      </OnboardProvider>
+    );
+  };
+
+  render(<Component />);
+
+  const message = await screen.findByTestId('message');
+  expect(message.textContent).toBe('this is a welcome with an element');
+
+  const highlightComponent = screen.getByTestId('highlight-component');
+  fireEvent.click(highlightComponent);
   expect(screen.queryByTestId('message')).toBeNull();
 });
